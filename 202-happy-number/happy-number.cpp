808c9f717323f58e9d4b2ab61d6ec1ba5c4fc788
prachichaudhary-18 <152068096+prachichaudhary-18@users.noreply.github.com>
2026-06-22 15:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.59%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+
+
+             while(n!=1 && n!=4){
+             int sum=0;
+            while(n>0){
+                int r=n%10;
+                sum+=r*r;
+                n/=10;
+            
+            }
+            n=sum;
+
+
+             }
+
+        
+        if(n==1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+     
+
+
+     
+
+
+        
+        
+    }
+};
